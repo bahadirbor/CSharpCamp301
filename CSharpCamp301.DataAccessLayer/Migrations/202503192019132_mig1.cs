7523a3d5c9acb107167119ecaca0c09a6c42d5ab
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-03-20 14:27:36 +0300

    Created migrations

---
+++ CSharpCamp301.DataAccessLayer/Migrations/202503192019132_mig1.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpCamp301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            //Update yaparsan bu işlem yapılacak
+            AddColumn("dbo.Customers", "CustomerStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            //İşlemden vazgeçersen bu işlem yapılacak
+            DropColumn("dbo.Customers", "CustomerStatus");
+        }
+    }
+}
